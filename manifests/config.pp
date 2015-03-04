a75f13ad3638659e021bd3dc9c6feeded93d63c2
Israel Calvete <icalvete@gmail.com>
2015-03-04 09:46:04 -0800

    Fixinf puppet-lint issues.

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,8 +7,8 @@ class mongodb::config {
   }
 
   exec{ 'config_bind_ip':
-    command => "/bin/sed -i -e \"s/bind_ip = .*/bind_ip = 0.0.0.0/\" $final_config_file",
-    unless  => "/bin/grep 'bind_ip = 0.0.0.0' $final_config_file"
+    command => "/bin/sed -i -e \"s/bind_ip = .*/bind_ip = 0.0.0.0/\" ${final_config_file}",
+    unless  => "/bin/grep 'bind_ip = 0.0.0.0' ${final_config_file}"
   }
 }
 
